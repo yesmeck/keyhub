45e0dd319bda980829873304b77f6bc7241b65f1
Wei Zhu <yesmeck@gmail.com>
2015-04-13 04:56:38 +0800

    Server api key

--- app/models/server_group.rb
+++ app/models/server_group.rb
@@ -2,4 +2,14 @@ class ServerGroup < ActiveRecord::Base
   belongs_to :project
 
   has_many :server_group_members
+
+  class << self
+    def find_by_api_key!(api_key)
+      find(Rails.application.message_verifier('server_group_api_key').verify(api_key))
+    end
+  end
+
+  def api_key
+    Rails.application.message_verifier('server_group_api_key').generate(id)
+  end
 end
